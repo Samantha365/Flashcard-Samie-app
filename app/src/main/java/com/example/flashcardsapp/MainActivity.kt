9698d30c83add82a944f887185f3ed8bb813d86f
unknown <Samantha Pierre>
2025-09-04 17:22:06 -0400

    first commit

---
+++ app/src/main/java/com/example/flashcardsapp/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.flashcardsapp
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.view.View
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val flashcardQuestion = findViewById<TextView>(R.id.flashcard_question)
+        val flashcardAnswer = findViewById<TextView>(R.id.flashcard_answer)
+        flashcardQuestion.setOnClickListener {
+            flashcardQuestion.visibility = View.INVISIBLE
+            flashcardAnswer.visibility = View.VISIBLE
+        }
+
+
+        flashcardAnswer.setOnClickListener {
+            flashcardQuestion.visibility=View.VISIBLE
+            flashcardAnswer.visibility=View.INVISIBLE
+        }
+    }
+}
